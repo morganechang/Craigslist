04621fb042c5aeb1ea44e882f82a94eb0fa39d27
Morgane Chang <morganechang@gmail.com>
2017-04-27 14:29:13 -0400

    + active, filters
    made boroughs active on click, activated filters, added
    data-anything-anything to temporarily store information on the browser
    (that has no other use but for dev, can’t be styled, but just way to
    reference info)

--- index.js
+++ index.js
@@ -1,19 +1,78 @@
 $(document).ready(function() {
 //getJSON = pull a request v. postJSON = save a request
+
 	$.ajax({
 	  method: "GET",
 	  url: "https://api.myjson.com/bins/2sadq?pretty=1",
 	  dataType: "json",
 	  success: function(response) {
-	  	
-	  	$.each(response.apartments, function(_index, apartment){  //_unusedObject 
-	  		var displayId = apartment.city.toLowerCase().replace(" ", "_");
+	  	window.data = response;
 
-	  		$(".apartments").append("<div class='list-group-item' id=" + displayId + "><h4 class='mb-1'>" + apartment.description + "/" + apartment.bedrooms + "/" + apartment.price + "</h4>" + apartment.address + "<br>" + apartment.city + "</div>");
-	  	})
-	  } 
+
+	  	$.each(response.apartments, function(_index, apartment){  //_unusedObject 
+	  		var displayId = apartment.city.toLowerCase().replace(" ", "-");
+
+	  		$(".apartments").append("<div data-apartment-id=" + apartment.id + " class='list-group-item listing' id=" + displayId + "><h4 class='mb-1'>" + apartment.description + "/" + apartment.bedrooms + "/" + apartment.price + "</h4>" + apartment.address + "<br>" + apartment.city + "</div>");	  	
+	  		//"data attributes" when no meaning; no need for ID or class; anything that begins "data"
+ 	  	})
+	  }
+
+	})
+
+	$(".filter").on("click", function(){
+		$(".filter").removeClass("active"); //"filter" custom class name
+		$(this).addClass("active");
+
+		var city = $(this).attr("id");
+
+		$(".listing").show();
+
+		if (city != "all") {
+			$(".listing").not("#" + city).hide();	// .not(ID!=city)		
+		};
+		
 	});
-});
+
+	$(".listing").on("click", function(){
+
+		$.each(window.data.apartments, function(_index, apartment){ 
+
+			if(apartment.id==("data-apartment-id")the real API response, which we dont have access to here, but wedo have access to which we storedin thewindow above. the id off of the element that was clicked)	
+			
+			var address = apartment.address (once we know that address has been confirmed)
+
+			window.open("http://maps.google.com/?q=" + address);
+		});
+
+	});
+
+});	
+
+
+// window.open("http://maps.google.com/?q=" + address);
+
+
+// 		for (var i=0; i < response.apartments.length; i++) {
+// 			apartments.city[i] = city
+// 			var cityCategory = $(".list-group-item")
+// 			if (city == )
+// 		};
+
+
+
+
+
+// function updateWordHint(letter) {  // this is a parameter and it will blow up if not called; shows that we are expecting something.
+// 		for (var i=0; i < wordArray.length; i++) {  // length = 8, but our i only goes up to 7
+// 			if (letter == wordArray[i]) {
+// 				wordHint[i] = letter;
+// 				fields = wordHint.toString().replace(/,/g," ");  //replace(/aA/g,"_")
+// 				$("#chosen-word").text(fields);	
+// 			} 
+// 		};
+// 	}
+
+
 
 
 
